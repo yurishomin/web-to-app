7a7c2b682a170a1e545a50ef92f4fc5da12f5836
Yuri Sh <58795166+yurishomin@users.noreply.github.com>
2026-03-18 03:21:46 +0300

    Substituted web site url

--- app/src/main/java/com/myexampoint/webtoapp/MainActivity.java
+++ app/src/main/java/com/myexampoint/webtoapp/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     String ShowOrHideWebViewInitialUse = "show";
     private WebView webview ;
     private ProgressBar spinner;
-    String myurl = "https://successtar.github.io"; //Change this  to your website hostname
+    String myurl = "https://ssetContentView//Change this  to your website hostname
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
